617ebbeb773a6980588c3cf126ab73ab481fc191
Doug Parker <dgp1130@users.noreply.github.com>
2020-01-18 16:58:00 -0800

    Adds an iframe to an extension page to render a sticky note.
    This required the URL to be marked "web accessible" in the manifest. I implemented this with basic Chrome extension APIs and without tests. I'll switch this over to WebExtensions and shard it to be more testable.
    
    I also made minor edits to `tslint` and `tsconfig`. I'm not sure why it isn't `strict` by default.

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,12 @@
+const injectedTabs = new Set<number>();
+
 // Listen for the extension icon button to be clicked.
 chrome.browserAction.onClicked.addListener((tab) => {
+  // Don't inject into a page which has already been injected.
+  if (!tab.id) throw new Error('Received tab with no ID');
+  if (injectedTabs.has(tab.id)) return;
+  injectedTabs.add(tab.id);
+
   // Inject the host script.
   chrome.tabs.executeScript(tab.id, {
     file: 'content-scripts/host.js',
@@ -9,4 +16,10 @@ chrome.browserAction.onClicked.addListener((tab) => {
   chrome.tabs.executeScript(tab.id, {
     file: 'runtime.js',
   });
+});
+
+// Remove the tab from the injected list when the user navigates and loses the
+// host script.
+chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+  injectedTabs.delete(details.tabId);
 });
--- src/content-scripts/host.ts
+++ src/content-scripts/host.ts
@@ -1,1 +1,29 @@
-console.log('I\'m the host!');
+// Execute in IIFE to allow early return.
+(() => {
+  if (document.querySelector('body > #stickit-container')) {
+    return; // Sticky note iframe already exits, don't do anything.
+  }
+
+  // Create a container to hold a sticky note.
+  const container = document.createElement('aside');
+  container.id = 'stickit-container';
+  container.style.position = 'absolute';
+  container.style.top = '0';
+  container.style.right = '0';
+  container.style.width = '400px';
+  container.style.height = '400px';
+  container.style.backgroundColor = 'yellow';
+  container.style.zIndex = '1000'; // 1000 'ought to be enough for any page.
+
+  // Create an iframe to link to load the Chrome extension content.
+  const iframe = document.createElement('iframe');
+  iframe.src = chrome.runtime.getURL('/index.html');
+  iframe.width = '100%';
+  iframe.height = '100%';
+  iframe.style.border = '0';
+  container.appendChild(iframe);
+
+  // Add the container to the document.
+  document.body.appendChild(container);
+})();
+
